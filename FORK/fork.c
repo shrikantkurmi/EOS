8c9b92fe3783618ee620f13a1295999d4861acfa
shrikantkurmi <shrikantkurmi4@gmail.com>
2021-11-30 08:55:51 -0800

    invoke the fork

---
+++ FORK/fork.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc,char *argv[])
+{
+    pid_t id;
+    printf("before fork\n");
+    id = fork();
+    printf("after fork\n");
+    return 0;
+}
