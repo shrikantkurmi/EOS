7ba2751c1930ca0937131fdd70f7a8e9d8e76ea5
shrikantkurmi <shrikantkurmi4@gmail.com>
2021-11-30 17:27:40 -0800

    modifiing forrk

--- FORK/fork.c
+++ FORK/fork.c
@@ -1,12 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
     pid_t id;
     printf("before fork\n");
     id = fork();
-    printf("after fork:%d\n",id);
+    printf("after fork:%d\n", id);
+
+    if (0 == id)
+    { 
+        printf("child\n");
+    }
+    else
+    {
+        printf("parent\n");
+    }    
     return 0;
 }
