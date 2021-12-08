053cd8d0a162c37d922558d55d4fdd4b59bc062d
shrikantkurmi <shrikantkurmi4@gmail.com>
2021-12-08 09:39:49 -0800

    message queue

--- Assignment11/p4.c
+++ Assignment11/p4.c
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[])
     cdacmq_attr.mq_msgsize = 128;
     cdacmq_attr.mq_curmsgs = 0;
 
-    cdacmq = mq_open(cdacmq,O_WRONLY | O_CREAT,S_IRUSR,S_IWUSR,&cdacmq_attr);
+    cdacmq = mq_open("/cdacmq",O_WRONLY | O_CREAT,S_IRUSR,S_IWUSR,&cdacmq_attr);
 
     mq_receive(cdacmq,buff,128,&msg_pri0);
     printf("Received msg:%s\nMsg pri0:%d\n",buff,msg_pri0);
